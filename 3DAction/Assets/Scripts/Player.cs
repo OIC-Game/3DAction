d5ee6d059de9081f2280395602810fc997094d1f
OIC-B9114-Towa <b9114@oic.jp>
2022-02-15 19:44:59 +0900

    コードの修正

--- 3DAction/Assets/Scripts/Player.cs
+++ 3DAction/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField] float speed = 0.15f;
     private Rigidbody rb;
